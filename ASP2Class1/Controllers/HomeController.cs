1df90776a17c860481739cb3b9f5aadae180b520
Justin Matte <justin.matte145@gmail.com>
2021-09-02 21:05:08 -0400

    Add project files.

---
+++ ASP2Class1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Tip15 = 0; 
+            ViewBag.Tip20 = 0; 
+            ViewBag.Tip25 = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalcModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Tip15 = model.CalculateTip15(); 
+                ViewBag.Tip20 = model.CalculateTip20(); 
+                ViewBag.Tip25 = model.CalculateTip25();
+            }
+            else
+            {
+                ViewBag.Tip15 = 0;
+                ViewBag.Tip20 = 0;
+                ViewBag.Tip25 = 0;
+            }
+            return View();
+        }
+    }
+}
---
+++ ASP2Class1/Models/TipCalcModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TipCalculator.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TipCalcModel
+    {
+        [Required(ErrorMessage = "Please enter a meal cost.")]
+        public decimal? MealCost { get; set; }
+
+        public decimal? Tip15 { get; set; }
+        public decimal? Tip20 { get; set; }
+        public decimal? Tip25 { get; set; }
+        public decimal? CalculateTip15()
+        {
+            Tip15 = MealCost * (15/100);
+            return Tip15;
+        }
+
+        public decimal? CalculateTip20()
+        {
+            Tip20 = MealCost * (20 / 100);
+            return Tip20;
+        }
+
+        public decimal? CalculateTip25()
+        {
+            Tip25 = MealCost * (25 / 100);
+            return Tip25;
+        }
+    }
+}
